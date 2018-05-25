82263fde8244a28b025b26a491cea51791f63d6e
superjomn <yanchunwei@outlook.com>
2018-05-25 08:17:06 +0800

    add scalar

--- pypage/_html.py
+++ pypage/_html.py
@@ -253,20 +253,31 @@ class Page(Tag):
     @page_switch_gstate
     def enable_bootstrap(self):
         with self:
-            State.gstate.append(
+            RawHtml(
                 '<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">'
             )
-            State.gstate.append(
+            RawHtml(
                 '<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>'
             )
-            State.gstate.append(
+            RawHtml(
                 '<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>'
             )
-            State.gstate.append(
+            RawHtml(
                 '<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>'
             )
         return self
 
+    @page_switch_gstate
+    def enable_echarts(self):
+        ''' Add pyecharts scripts '''
+        host = "https://pyecharts.github.io/assets/js"
+        with self:
+            RawHtml('''
+            {% for jsfile_name in script_list %}
+                <script src="%s/{{ jsfile_name }}.js"></script>
+            {% endfor %}
+            ''' % host)
+        return self
 
 def compile(li):
     '''
